5ff2b6e7e672e3834fe23860a6d1268b7387a092
miiro623 <wogur3562@naver.com>
2024-06-03 01:25:27 +0900

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,5 +38,10 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    implementation ("com.squareup.retrofit2:retrofit:2.6.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.6.0'")
+    implementation ("com.google.code.gson:gson:2.8.6")
+    implementation ("com.squareup.okhttp3:logging-interceptor:3.11.0'")
+
 
 }
